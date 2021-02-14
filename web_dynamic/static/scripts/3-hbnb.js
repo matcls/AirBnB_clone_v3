76f9c2cd9cd1db3984180927d74b2bfe1f104639
matcls <manueltorresvesga@gmail.com>
2021-02-14 16:00:51 -0500

    Add task 4. Fetch places
    Replace the route 2-hbnb with 3-hbnb in the file 3-hbnb.py (based on 2-hbnb.py)
    Create a new template 3-hbnb.html (based on 2-hbnb.html) and update it:
    Import the JavaScript static/scripts/3-hbnb.js in the <HEAD> tag (instead of 2-hbnb.js)
    Remove the entire Jinja section of displaying all places (all ARTICLE tags)
    Write a JavaScript script (static/scripts/3-hbnb.js):
          Based on 2-hbnb.js
          Request http://0.0.0.0:5001/api/v1/places_search/:
    The final result must be the same as previously, but now,
    places are loaded from the front-end, not from the back-end!

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,64 @@
+$(function () {
+  const objeto = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      // $(this) has a value of the html child in this case is input
+      // to get the value of a tag html inside of input we use the method attr()
+      objeto[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete objeto[$(this).attr('data-id')];
+    }
+    console.log(objeto);
+    let arr = '';
+    let separator = '';
+    for (const i in objeto) {
+      arr += separator;
+      arr += objeto[i];
+      separator = ', ';
+    }
+    $('div.amenities h4').text(arr);
+  });
+getApiStatus();
+getPlaces();
+});
+
+function getApiStatus() {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+        }
+      }
+    }
+    );
+}
+
+function getPlaces () {
+  $.ajax(
+    'http://localhost:5001/api/v1/places_search/', {
+    data: JSON.stringify({}),
+    headers: { 'Content-Type': 'application/json' },
+    type: 'POST',
+    success: data => {
+      for (const place of data) {
+        const article = ['<article>',
+        '<div class="title_box">',
+          `<h2>${place.name}</h2>`,
+          `<div class="price_by_night">$${place.price_by_night}</div>`,
+        '</div>',
+        '<div class="information">',
+          `<div class="max_guest">${place.max_guest} Guest(s)</div>`,
+          `<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>`,
+          `<div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>`,
+        '</div>',
+        '<div class="description">',
+          `${place.description}`,
+        '</div>',
+        '</article>'];
+        $('SECTION.places').append(article.join(''));
+      }
+    },
+  });
+}
